fbc9d919c3f058c35dde7142b1c4369640036228
Charles Cognato <charlescognato@gmail.com>
2014-11-12 21:31:38 -0500

    added a sequence test for both even and odd

--- net/flyboy8869/tests/test_even.py
+++ net/flyboy8869/tests/test_even.py
@@ -8,3 +8,8 @@ class TestEvenFunction(unittest.TestCase):
 
     def test_even(self):
         self.assertTrue(even(2), "2 is an even number, so this should be 'True'")
+
+    def test_sequence_even(self):
+        l = [0, 2, 4, 6, 8, 10]
+        self.assertEqual(l, [i for i in range(11) if even(i)], "Sequences don't match")
+
--- net/flyboy8869/tests/test_odd.py
+++ net/flyboy8869/tests/test_odd.py
@@ -8,3 +8,7 @@ class TestEvenFunction(unittest.TestCase):
 
     def test_odd(self):
         self.assertTrue(odd(3), "3 is an odd number, so this should be 'True'")
+
+    def test_sequence_odd(self):
+        l = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+        self.assertEqual(l, [i for i in range(22) if odd(i)], "Sequences are not equal")
